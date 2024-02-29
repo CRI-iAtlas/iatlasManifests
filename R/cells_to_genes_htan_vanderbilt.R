6b3c8ff490e360b1f3f531511f7ae0b3f0c67e33
heimannch <carolina.heimann@gmail.com>
2024-02-29 11:43:30 -0800

    add cell to genes tables

---
+++ R/cells_to_genes_htan_vanderbilt.R
@@ -0,0 +1,48 @@
+cells_to_genes_htan_vanderbilt <- function() {
+
+  require(magrittr)
+  require(rlang)
+  syn <- create_synapse_login()
+#TODO: store new genes
+
+    #get cell ids
+    cells_ids <- synapse_csv_id_to_tbl(syn, "syn53701120") %>%
+      dplyr::select(
+        "Cell" = "name",
+        "cell_id" = "id"
+      )
+
+    #get gene info
+    tcga_genes <- synapse_csv_id_to_tbl(syn, "syn50896922")
+    htan_genes <- synapse_csv_id_to_tbl(syn, "syn53710212")
+
+    #we will only add genes that are already in the iAtlas database
+    genes_to_add <- tcga_genes[tcga_genes$hgnc_id %in% htan_genes$feature_name,  c("hgnc_id", "id")]
+
+    #get expression values
+    htan_expr <- synapse_csv_id_to_tbl(syn, "syn53710204")
+
+    cells_to_genes <- htan_expr %>%
+      dplyr::rename("Cell" = "...1") %>%
+      dplyr::select(Cell, dplyr::all_of(genes_to_add$hgnc_id)) %>%
+      dplyr::inner_join(cells_ids, by = "Cell") %>%
+      tidyr::pivot_longer(-c(Cell, cell_id), names_to = "hgnc_id", values_to = "single_cell_seq") %>%
+      dplyr::filter(single_cell_seq != 0) %>%
+      dplyr::inner_join(genes_to_add, by = "hgnc_id", relationship = "many-to-many") %>%
+      dplyr::select(
+        "cell_id",
+        "gene_id" = "id",
+        "single_cell_seq") %>%
+      dplyr::mutate(
+        "id" = uuid::UUIDgenerate(n = dplyr::n()),
+        "Component" = "cells_to_genes"
+      )
+
+  synapse_store_table_as_csv(
+    syn,
+    cells_to_genes,
+    "syn53708096",
+    "cells_to_genes"
+  )
+
+}
