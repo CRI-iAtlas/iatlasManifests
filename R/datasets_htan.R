f51a826ac3112d7785f913090d3a7f49e4d5e2b1
Carolina Heimann <32350126+heimannch@users.noreply.github.com>
2024-09-04 15:43:09 -0700

    Update datasets_htan.R

--- R/datasets_htan.R
+++ R/datasets_htan.R
@@ -5,19 +5,13 @@ dataset_htan <- function(){
 
   datasets <-
     dplyr::tibble("display" = character(), "dataset_type" = character()) %>%
-    dplyr::add_row("display" = c("HTAN OHSU"), "dataset_type" = "analysis") %>%
+    dplyr::add_row("display" = c("HTAN OHSU"), "dataset_type" = "ici") %>%
     dplyr::mutate("name" = stringr::str_replace_all(.data$display, " ", "_")) %>%
     dplyr::arrange(.data$name) %>%
     dplyr::mutate(
       "id" = uuid::UUIDgenerate(n = dplyr::n()),
       "Component" = "datasets"
     )
-  # synapse_store_table_as_csv(
-  #   syn,
-  #   ici_datasets,
-  #   "", #replace
-  #   "datasets"
-  # )
 
 }
 
